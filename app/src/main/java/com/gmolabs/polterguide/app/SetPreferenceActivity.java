8adaec9609cf60d9c44016678670356f088a3c92
Geoff Morris <geoff@cycling74.com>
2014-07-13 09:42:25 -0700

    Firebase now updates with new username/password when changes are set in preferences.

--- app/src/main/java/com/gmolabs/polterguide/app/SetPreferenceActivity.java
+++ app/src/main/java/com/gmolabs/polterguide/app/SetPreferenceActivity.java
@@ -3,11 +3,14 @@ package com.gmolabs.polterguide.app;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import com.firebase.client.Firebase;
 
 /**
  * Created by geoffmorris on 7/5/14.
  */
-public class SetPreferenceActivity extends Activity {
+public class SetPreferenceActivity extends Activity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     SharedPreferences p;
 
@@ -19,8 +22,33 @@ public class SetPreferenceActivity extends Activity {
         getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new PrefsFragment()).commit();
 
-//        p = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        p = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 //        p.getString(R.string.username, value);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Set up a listener whenever a key changes
+        p.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Unregister the listener whenever a key changes
+        p.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,String key)
+    {
+        String myUser = sharedPreferences.getString("username", "DEFAULT");
+        String myPass = sharedPreferences.getString("password", "DEFAULT");
+        Firebase usersRef = new Firebase("https://polterguide.firebaseio.com/users/");
+        Firebase userRef = usersRef.child(myUser);
+        userRef.child("pwd").setValue(myPass);
+        // do stuff
+    }
+
 
 }
